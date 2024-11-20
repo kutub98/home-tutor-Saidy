3a6ac23cac4dab6e6b18783c3b989ee994009d9d
Kutub Udd <socialnetwork98710@gmail.com>
2024-11-20 17:59:20 +0600

    added tuition component in pannel

--- src/app/(dashaboardLayout)/admin/addTuition/page.tsx
+++ src/app/(dashaboardLayout)/admin/addTuition/page.tsx
@@ -50,7 +50,8 @@ const AddTuition = () => {
           <DialogHeader>
             <DialogTitle>Edit profile</DialogTitle>
             <DialogDescription>
-              Make changes to your profile here. Click save when you're done.
+              Make changes to your profile here. Click save when {"you're"}
+              done.
             </DialogDescription>
           </DialogHeader>
           <div className="grid gap-4 py-4">
